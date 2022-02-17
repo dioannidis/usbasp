9f65f23f79767e0e0865fa48e5e8ae25c0c43d7e
Dimitrios Chr. Ioannidis <d.ioannidis@nephelae.eu>
2022-02-17 14:40:57 +0200

    Clean up leftovers.

--- firmware/usbasp.h
+++ firmware/usbasp.h
@@ -37,9 +37,6 @@
 
 /* USBASP capabilities */
 #define USBASP_CAP_0_TPI                0x01
-
-/* USBASP capabilities */
-#define USBASP_CAP_0_TPI                0x01
 #define USBASP_CAP_6_UART               0x40
 #define USBASP_CAP_20MHZ_CLOCK          0x01
 
