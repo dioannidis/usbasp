97350b3bd27ef01995140eaf2f897949dba6c8e0
Dimitrios Chr. Ioannidis <d.ioannidis@nephelae.eu>
2023-03-15 18:28:31 +0200

    Configure the Serial Number Desscriptor to be read from EEPROM

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -257,8 +257,8 @@ section at the end of this file).
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
  */
-#define USB_CFG_SERIAL_NUMBER   '0', '0', '0', '1' 
-#define USB_CFG_SERIAL_NUMBER_LEN   4 
+/* #define USB_CFG_SERIAL_NUMBER   '0', '0', '0', '1' */
+/* #define USB_CFG_SERIAL_NUMBER_LEN   4 */
 /* Same as above for the serial number. If you don't want a serial number,
  * undefine the macros.
  * It may be useful to provide the serial number through other means than at
@@ -350,7 +350,7 @@ section at the end of this file).
 #define USB_CFG_DESCR_PROPS_STRING_0                0
 #define USB_CFG_DESCR_PROPS_STRING_VENDOR           0 
 #define USB_CFG_DESCR_PROPS_STRING_PRODUCT          0
-#define USB_CFG_DESCR_PROPS_STRING_SERIAL_NUMBER    0
+#define USB_CFG_DESCR_PROPS_STRING_SERIAL_NUMBER    (USB_PROP_IS_EEPROM | USB_PROP_EEPROM_STRING_LENGTH(4))
 #define USB_CFG_DESCR_PROPS_HID                     0
 #define USB_CFG_DESCR_PROPS_HID_REPORT              0
 #define USB_CFG_DESCR_PROPS_UNKNOWN                 USB_PROP_IS_DYNAMIC
