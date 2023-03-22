081f263b6097b233b8227ba2e6cb7d4cf472b110
Dimitrios Chr. Ioannidis <d.ioannidis@nephelae.eu>
2023-03-22 11:00:29 +0200

    Add comments

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -295,22 +295,29 @@ section at the end of this file).
 /* ------------------- Fine Control over USB Descriptors ------------------- */
 /* If you don't want to use the driver's default USB descriptors, you can
  * provide our own. These can be provided as (1) fixed length static data in
- * flash memory, (2) fixed length static data in RAM or (3) dynamically at
- * runtime in the function usbFunctionDescriptor(). See usbdrv.h for more
- * information about this function.
+ * flash memory, (2) fixed length static data in RAM, (3) dynamically at
+ * runtime in the function usbFunctionDescriptor(), or (4) fixed length static
+ * data in EEPROM. See usbdrv.h for more information about this function.
  * Descriptor handling is configured through the descriptor's properties. If
  * no properties are defined or if they are 0, the default descriptor is used.
  * Possible properties are:
  *   + USB_PROP_IS_DYNAMIC: The data for the descriptor should be fetched
  *     at runtime via usbFunctionDescriptor(). If the usbMsgPtr mechanism is
  *     used, the data is in FLASH by default. Add property USB_PROP_IS_RAM if
- *     you want RAM pointers.
+ *     you want RAM pointers or add property USB_PROP_IS_EEPROM if
+ *     the data is in EEPROM.
  *   + USB_PROP_IS_RAM: The data returned by usbFunctionDescriptor() or found
  *     in static memory is in RAM, not in flash memory.
+ *   + USB_PROP_IS_EEPROM: The data returned by usbFunctionDescriptor() or found
+ *     in static memory is in EEPROM, not in flash memory or in RAM.
  *   + USB_PROP_LENGTH(len): If the data is in static memory (RAM or flash),
  *     the driver must know the descriptor's length. The descriptor itself is
  *     found at the address of a well known identifier (see below).
- * List of static descriptor names (must be declared PROGMEM if in flash):
+ *   + USB_PROP_EEPROM_STRING_LENGTH(len): If the data is in EEPROM,
+ *     the driver must know the descriptor's length. The descriptor itself is
+ *     found at the address of a well known identifier (see below).
+ * List of static descriptor names (must be declared PROGMEM if in flash 
+ *   or EEMEM if in EEPROM):
  *   char usbDescriptorDevice[];
  *   char usbDescriptorConfiguration[];
  *   char usbDescriptorHidReport[];
