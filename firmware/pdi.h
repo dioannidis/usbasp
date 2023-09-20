2c2778a73b9bb2b1390e6aa1d2847eb29df97d1a
Dimitrios Chr. Ioannidis <d.ioannidis@nephelae.eu>
2023-09-20 21:00:41 +0300

    Some cleanup

--- firmware/pdi.h
+++ firmware/pdi.h
@@ -26,14 +26,6 @@
 #define PDI_MISO  PB4
 #define PDI_SCK   PB5
 
-
-extern uchar pdi_active;
-extern uchar pdi_interrupted;
-extern uchar pdi_interrupt_count;
-extern uchar pdi_se0_count;
-extern uchar pdi_rx;
-extern uchar pdi_nvmbusy;
-
 #define PDI_STATUS_OK 0
 #define PDI_STATUS_TIMEOUT 1
 #define PDI_STATUS_PARITY 2
@@ -41,20 +33,16 @@ extern uchar pdi_nvmbusy;
 #define PDI_STATUS_NVM_TIMEOUT 4
 #define PDI_STATUS_COLLISION 5
 
+/* 64 Idle Bits ( 8 Bytes ) */
 #define PDI_GUARD_TIME 1
 
+extern uchar pdi_nvmbusy;
 extern volatile uchar pdiState;
 
 uchar pdiConnect();
 void pdiDisconnect(uchar keep_reset);
 uchar pdiEnterProgrammingMode();
 uchar pdiWaitNVM();
-uchar pdiReadBlock(uint32_t addr,uchar* data,uchar len);
-uchar byteParity(uchar b);
-void pdiSendByte(uchar b);
-void pdiSendBytes(uchar* ptr,uchar count);
-void pdiSendByteX(uchar b,uchar extra);
-uchar pdiReadCtrl(uint32_t addr, uchar *value);
-uchar pdiResetDev(uchar reset);
+uchar pdiReadBlock(uint32_t address, uchar* dataBuf, uchar lenAsked);
 
 #endif
